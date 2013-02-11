76cb48c89b65436fdbb69168dea1353f4ad6085e
Arnaud DEMARCQ <arnaud.demarcq@laposte.net>
2013-02-11 18:56:18 +0100

    fixes Test Class to match new Scripts

--- TagContainer/src/main/java/org/krohm/tagcontainer/servlet/account/TestGetAccountJavascriptServlet.java
+++ TagContainer/src/main/java/org/krohm/tagcontainer/servlet/account/TestGetAccountJavascriptServlet.java
@@ -29,8 +29,8 @@ public class TestGetAccountJavascriptServlet extends AbstractGetAccountJavascrip
     protected List<String> getUrlList(HttpServletRequest request) {
         List<String> returnList = new ArrayList<String>();
         returnList.add("http://code.jquery.com/jquery.min.js");
-        returnList.add("./GetJavascript/1528795644/script.js");
-        returnList.add("./GetJavascript/9876543210/script.js?thisis=aparam&thatis=anotherone");
+        returnList.add(request.getContextPath() + "/GetJavascript/Static/1528795644/script.js");
+        returnList.add(request.getContextPath() + "/GetJavascript/Dynamic/9876543210/script.js?thisis=aparam&thatis=anotherone");
         returnList.add("http://dummy.not.existing/script.js");
         return returnList;
 
